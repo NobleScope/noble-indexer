2d78b77ff1e8bf2fdd7a1f6f0aea469020687747
Mikhail Smirnov <58223117+mismirnov@users.noreply.github.com>
2026-01-26 20:04:53 +0300

    Fix: api blocks sort (#35)

--- internal/storage/postgres/block.go
+++ internal/storage/postgres/block.go
@@ -54,6 +54,7 @@ func (b *Block) Filter(ctx context.Context, fltrs storage.BlockListFilter) (bloc
 	query := b.DB().NewSelect().
 		Model(&blocks)
 
+	query = sortTimeIDScope(query, fltrs.Sort)
 	query = limitScope(query, fltrs.Limit)
 	query = query.Offset(fltrs.Offset)
 
