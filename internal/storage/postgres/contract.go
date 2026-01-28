7759d5e08e3688d8ce30894fb52b09270665d608
mismirnov <mi.smirnov24@gmail.com>
2026-01-28 17:39:34 +0300

    Refactoring: contract by hash

--- internal/storage/postgres/contract.go
+++ internal/storage/postgres/contract.go
@@ -56,16 +56,13 @@ func (c *Contract) ListWithTx(ctx context.Context, filters storage.ContractListF
 
 // ByHash -
 func (c *Contract) ByHash(ctx context.Context, hash pkgTypes.Hex) (contract storage.Contract, err error) {
-	query := c.DB().NewSelect().
-		Model((*storage.Contract)(nil))
-
 	err = c.DB().NewSelect().
-		TableExpr("(?) AS contract", query).
+		Model((*storage.Address)(nil)).
 		ColumnExpr("contract.*").
 		ColumnExpr("address.id AS address__id, address.first_height AS address__first_height, address.last_height AS address__last_height, address.hash AS address__hash, address.is_contract AS address__is_contract, address.txs_count AS address__txs_count, address.contracts_count AS address__contracts_count, address.interactions AS address__interactions").
 		ColumnExpr("tx.hash AS tx__hash").
 		ColumnExpr("implementation_address.hash AS implementation").
-		Join("JOIN address ON address.id = contract.id").
+		Join("JOIN contract ON contract.id = address.id").
 		Join("LEFT JOIN proxy_contract ON proxy_contract.id = contract.id").
 		Join("LEFT JOIN address AS implementation_address ON implementation_address.id = proxy_contract.implementation_id").
 		Join("LEFT JOIN tx ON contract.tx_id = tx.id").
