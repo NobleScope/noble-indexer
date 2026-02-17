0ae12389eba1d504d07abd3c02b2c8731495d393
mismirnov <mi.smirnov24@gmail.com>
2026-02-17 09:39:55 +0300

    Test fix;

--- internal/storage/postgres/verification_file_test.go
+++ internal/storage/postgres/verification_file_test.go
@@ -145,7 +145,7 @@ func (s *StorageTestSuite) TestVerificationFileCountsByTask() {
 	}
 }
 
-func (s *StorageTestSuite) TestVerificationFileBulkSaveAndRead() {
+func (s *StorageTestSuite) TestVerificationFileZBulkSaveAndRead() {
 	ctx, ctxCancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer ctxCancel()
 
